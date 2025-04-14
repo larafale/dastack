e9b4a56e866198fc159a391500aee3f369371c3e
louis <louis.grellet@gmail.com>
2025-04-14 12:07:30 +0200

    dark prose

--- src/app/docs/shared.tsx
+++ src/app/docs/shared.tsx
@@ -2,7 +2,7 @@
 
 export function DocContent({ children }: { children: React.ReactNode }) {
     return (
-        <div className="max-w-2xl prose">
+        <div className="max-w-2xl prose dark:prose-invert">
             {children}
         </div>
     )
